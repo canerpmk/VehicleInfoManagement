31103e9b0069b0fab972c0b6c40700708abdf33d
canerpmk <86322716+canerpmk@users.noreply.github.com>
2023-08-28 12:23:07 +0300

    ParseServiceTest.java is added

---
+++ src/test/java/com/example/sahibinden/service/impl/ParseServiceTest.java
@@ -0,0 +1,53 @@
+package com.example.sahibinden.service.impl;
+
+import com.example.sahibinden.controller.PaketControllerTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParseServiceTest {
+
+    @Test
+    void updateMarkas() {
+    }
+
+    @Test
+    void parseMarkaPage() {
+    }
+
+    @Test
+    void updateModels() {
+    }
+
+    @Test
+    void parseModelPage() {
+    }
+
+    @Test
+    void updateKasas() {
+    }
+
+    @Test
+    void parseKasaPage() {
+    }
+
+    @Test
+    void parseKasaaPage() {
+    }
+
+    @Test
+    void updateMotors() {
+    }
+
+    @Test
+    void parseMotorPage() {
+    }
+
+    @Test
+    void parseMotorrPage() {
+    }
+
+    @Test
+    void parseOzellikPage() {
+    }
+}
